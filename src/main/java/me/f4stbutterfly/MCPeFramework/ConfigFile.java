121f47b4bf1173f0d91853e3bf4990fdaa5ae5d1
FastButterFly <szymon1.github@gmail.com>
2026-05-01 10:44:23 +0200

    Config file

---
+++ src/main/java/me/f4stbutterfly/MCPeFramework/ConfigFile.java
@@ -0,0 +1,28 @@
+package me.f4stbutterfly.MCPeFramework;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigFile {
+
+	private final JavaPlugin plugin;
+	private File file;
+	public FileConfiguration data;
+
+	public ConfigFile(JavaPlugin p, String configFileName) throws IOException {
+		this.plugin = p;
+		loadConfig(configFileName);
+	}
+
+	private void loadConfig(String path) throws IOException {
+		file = new File(plugin.getDataFolder(), path);
+		if(!file.exists()) {
+			plugin.saveResource(path, false);
+		}
+		data = YamlConfiguration.loadConfiguration(file);
+	}
+}
